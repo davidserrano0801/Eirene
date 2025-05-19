6922f1adf9843173746bb971d0be6b52fd313e54
davidserrano0801 <davidserrano0801@gmail.com>
2025-05-19 18:11:48 -0500

    Update main.dart

--- eirene_app/lib/main.dart
+++ eirene_app/lib/main.dart
@@ -31,22 +31,42 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  @override
+   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      body: Container(
-        child: Container(
-          child: Text('Bienvenido:',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color.fromRGBO(0, 133, 182, 1),
-            fontFamily: 'Comfortaa',
-            fontSize: 56,
-            fontWeight: FontWeight.w300
-          ),)
-        )
-      )
-    );   
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Bienvenido:',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: const Color.fromRGBO(0, 133, 182, 1),
+                fontFamily: 'Comfortaa',
+                fontSize: 56,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Image.asset('assets/imgs/logo_homepage.png'),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Usuario UIS'
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Contraseña'
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
